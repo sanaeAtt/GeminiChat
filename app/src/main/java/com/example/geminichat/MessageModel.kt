6ab9993e846bfc27c7b7713cc984f8e806b5e7e8
AATIK Sanae <93257453+sanaeAtt@users.noreply.github.com>
2024-07-01 16:18:31 +0100

    Reapply "historique avec division // auj- hier ..."
    This reverts commit 9928b476340fc3ab92b92574d1c3abf91c291f38.

--- app/src/main/java/com/example/geminichat/MessageModel.kt
+++ app/src/main/java/com/example/geminichat/MessageModel.kt
@@ -1,7 +1,10 @@
 
 package com.example.geminichat
 
+import java.time.LocalDateTime
+
 data class MessageModel(
     val message : String,
-    val rool: String
+    val rool: String,
+    val timestamp: LocalDateTime = LocalDateTime.now()
 )
